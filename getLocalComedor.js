9b2d3febbafeb5456f9458fa43916c623950d674
luen <luis.mincitar@informexti.com>
2024-04-27 11:31:48 -0600

    local updates p1

--- getLocalComedor.js
+++ getLocalComedor.js
@@ -2,8 +2,8 @@
 const mysql = require("mysql");
 const fs = require('fs');
 const csvWriter = require("csv-write-stream");
-const writer = csvWriter({headers: ['sagarpa', 'ruta', 'huerto', 'productor', 'ubicacion', 'encargado']});//cabeceras del archivo, comprobar nombres
-const fileStream = fs.createWriteStream("G:/Mi unidad/Huertos-157755893/data.csv")//directorio donde se guardara el archivo, comprobar ruta
+const writer = csvWriter({headers: ['limpio', 'agua', 'fugas','jabon','toallas']});//cabeceras del archivo, comprobar nombres
+const fileStream = fs.createWriteStream("G:/Mi unidad/Huertos-157755893/localComedor.csv")//directorio donde se guardara el archivo, comprobar ruta
 
 const connection = mysql.createConnection({
     host:"localhost",//direccion IP del servidor    
@@ -20,7 +20,7 @@ connection.connect((err) =>{
 
 
 
-connection.query("SELECT * FROM  evaluacionhuertoslimpios___datossanidad", (err, results) => {//consulta SQL, sera proporcionanda por jaime
+connection.query("SELECT * FROM  inspecciones", (err, results) => {//consulta SQL, sera proporcionanda por jaime
     if (err) throw err;
     console.log("datos:", results)
     
@@ -29,7 +29,7 @@ connection.query("SELECT * FROM  evaluacionhuertoslimpios___datossanidad", (err,
     writer.pipe(fileStream);
     console.log("Escribiendo")
     results.forEach((row) => {
-        writer.write([row.rSader, row.rutaID, row.huerto, row.productor, row.ubicacion, row.rutaIng]);//constructor del archivo, adecuar a los datos de la consulta sql
+        writer.write([row.sanitarioLimpio, row.aguaPotable, row.fugas,row.jabon,row.taollas]);//constructor del archivo, adecuar a los datos de la consulta sql
     });
     writer.end();
     connection.end();
@@ -37,5 +37,5 @@ connection.query("SELECT * FROM  evaluacionhuertoslimpios___datossanidad", (err,
 
 
 
-console.log("CSV guardado")
+console.log("CSV guardado - comedor")
 
